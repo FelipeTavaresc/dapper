72c382646223e5e0eeb5c2a5e155086965e41f1a
José Felipe Tavares Costa <t.jose.costa@estapar.com.br>
2024-04-22 09:15:33 -0300

    Utilizando SqlCommand e SqlDataReader

--- modulo-1/BaltaDataAccess/Program.cs
+++ modulo-1/BaltaDataAccess/Program.cs
@@ -7,6 +7,26 @@ namespace BaltaDataAccess
         static void Main(string[] args)
         {
             const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                Console.WriteLine("Conecatado");
+                connection.Open();
+                
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+                    }
+                }
+            }
+
             Console.WriteLine("Hello, World!");
         }
     }
