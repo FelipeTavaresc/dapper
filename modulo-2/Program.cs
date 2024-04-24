004d9c5cd266ed5e03129a72cf058bb92635656c
Felipe Tavares <felipe.tavares@outlook.com>
2024-04-24 05:59:08 -0300

    Insert via Dapper

--- modulo-2/Program.cs
+++ modulo-2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using BaltaDataAccess.Models;
 using Dapper;
 using Microsoft.Data.SqlClient;
+using System;
 
 namespace BaltaDataAccess
 {
@@ -10,12 +11,44 @@ namespace BaltaDataAccess
         {
             const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
 
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Description = "Categoria destinada a serviços do AWS";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Featured = false;
+
+            var insertSql = @"INSERT INTO 
+                    [Category] 
+                VALUES(
+                    @Id, 
+                    @Title, 
+                    @Url, 
+                    @Summary, 
+                    @Order, 
+                    @Description, 
+                    @Featured)";
+
+
             using (var connection = new SqlConnection(connectionString))
             {
+                var rows = connection.Execute(insertSql, new{
+                    category.Id,
+                    category.Title,
+                    category.Url,
+                    category.Summary,
+                    category.Order,
+                    category.Description,
+                    category.Featured
+                });
+                Console.WriteLine($"{rows} linha(s) inserida(s)");
+
                 var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-                foreach (var category in categories)
+                foreach (var item in categories)
                 {
-                    Console.WriteLine($"{category.Id} - {category.Title}");
+                    Console.WriteLine($"{item.Id} - {item.Title}");
                 }
             }
 
