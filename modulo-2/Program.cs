4919a63a3ea4ff3062aef75aa6824929943037fa
Felipe Tavares <felipe.tavares@outlook.com>
2024-04-24 05:34:27 -0300

    Adicionando modulo 2

---
+++ modulo-2/Program.cs
@@ -0,0 +1,29 @@
+﻿using BaltaDataAccess.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace BaltaDataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+                foreach (var category in categories)
+                {
+                    Console.WriteLine($"{category.Id} - {category.Title}");
+                }
+            }
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+
+
+
