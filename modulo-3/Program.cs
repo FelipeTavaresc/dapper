29a2393165c1dfe8e637516e699f2388c406e849
Felipe Tavares <felipe.tavares@outlook.com>
2024-05-01 05:33:03 -0300

    Select IN

--- modulo-3/Program.cs
+++ modulo-3/Program.cs
@@ -25,7 +25,8 @@ namespace BaltaDataAccess
                 // ReadView(connection);
                 // OneToOne(connection);
                 // OneToMany(connection);
-                QueryMultiple(connection);
+                // QueryMultiple(connection);
+                SelectIn(connection);
             }
         }
 
@@ -274,6 +275,23 @@ namespace BaltaDataAccess
             }
         }
 
+        static void SelectIn(SqlConnection connection)
+        {
+            var query = @"SELECT * FROM Career WHERE [Id] IN @Id";
+
+            var items = connection.Query<Career>(query, new
+            {
+                Id = new[]{
+                    "4327ac7e-963b-4893-9f31-9a3b28a4e72b",
+                    "e6730d1c-6870-4df3-ae68-438624e04c72"
+                }
+            });
+
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.Title);
+            }
+        }
     }
 }
 
