6ab6be9712f2b15ccd5aad13e7e09d5f48aa35a7
Felipe Tavares <felipe.tavares@outlook.com>
2024-05-03 06:22:06 -0300

    Adicionando RoleRepository

--- modulo-4/Program.cs
+++ modulo-4/Program.cs
@@ -12,11 +12,10 @@ namespace Blog
         {
             var connection = new SqlConnection(CONNECTION_STRING);
             connection.Open();
-            // ReadUsers();
-            // ReadUser();
-            // CreateUser();
-            // UpdateUser();
-            DeleteUser();
+
+            //ReadUsers(connection);
+            ReadRoles(connection);
+            
             connection.Close();
         }
 
@@ -30,62 +29,15 @@ namespace Blog
 
         }
 
-        public static void ReadUser()
+        public static void ReadRoles(SqlConnection connection)
         {
-            using (var connection = new SqlConnection(CONNECTION_STRING))
-            {
-                var user = connection.Get<User>(1);
-                Console.WriteLine(user.Name);
-            }
+            var repository = new RoleRepository(connection);
+            var roles = repository.Get();
+
+            foreach (var role in roles)
+                Console.WriteLine(role.Name);
+
         }
-
-        public static void CreateUser()
-        {
-            var user = new User()
-            {
-                Bio = "Equipe Balta",
-                Email = "equipe@balta.io",
-                Image = "https://imagem",
-                Name = "Equipe balta.io",
-                PasswordHash = "HASH",
-                Slug = "equipe-balta"
-            };
-
-            using (var connection = new SqlConnection(CONNECTION_STRING))
-            {
-                connection.Insert<User>(user);
-                Console.WriteLine("Cadastro realizado com sucesso.");
-            }
-        }
-
-        public static void UpdateUser()
-        {
-            var user = new User()
-            {
-                Id = 2,
-                Bio = "Equipe | Balta",
-                Email = "equipe@balta.io",
-                Image = "https://imagem",
-                Name = "Equipe de Suporte balta.io",
-                PasswordHash = "HASH",
-                Slug = "equipe-balta"
-            };
-
-            using (var connection = new SqlConnection(CONNECTION_STRING))
-            {
-                connection.Update<User>(user);
-                Console.WriteLine("Atualização realizada com sucesso.");
-            }
-        }
-
-        public static void DeleteUser()
-        {
-            using (var connection = new SqlConnection(CONNECTION_STRING))
-            {
-                var user = connection.Get<User>(2);
-                connection.Delete<User>(user);
-                Console.WriteLine("Exclusão realizada com sucesso.");
-            }
-        }
+       
     }
 }
---
+++ modulo-4/Repositories/RoleRepository.cs
@@ -0,0 +1,19 @@
+using Microsoft.Data.SqlClient;
+using Blog.Models;
+using Dapper.Contrib.Extensions;
+
+namespace Blog.Repositories
+{
+    public class RoleRepository
+    {
+        private readonly SqlConnection _connection;
+
+        public RoleRepository(SqlConnection connection) => _connection = connection;
+
+        public IEnumerable<Role> Get() => _connection.GetAll<Role>();
+
+        public Role Get(int id) => _connection.Get<Role>(id);
+
+        public void Create(Role role) => _connection.Insert<Role>(role);
+    }
+}
