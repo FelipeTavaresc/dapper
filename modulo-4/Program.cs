d1f06f6865b53f66a21b695fed3c06aaff4aae5b
Felipe Tavares <felipe.tavares@outlook.com>
2024-05-03 05:25:15 -0300

    Utilizando Dapper Contrib

--- modulo-4/Program.cs
+++ modulo-4/Program.cs
@@ -6,7 +6,7 @@ namespace Blog
 {
     class Program
     {
-        private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$";
+        private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$; TrustServerCertificate=True";
         static void Main(string[] args)
         {
             ReadUsers();
